ffb0eb82fd7d53cca19e1bb5348820de0cd5161b
jacksonspenc <jacksonspencer327@gmail.com>
2024-05-09 18:50:25 -0600

    first commit

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,5 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
+    //create the variable
+    public List<JournalEntry> _journal = new List<JournalEntry>();
+    private string fileName = "MyJournal.txt";
+
+    
     public List<Entry>_entries;
     {
 
@@ -7,16 +16,56 @@ public class Journal
 
     public void DisplayAll()
     {
-
+        Console.WriteLine("\n************** Journal Entries **************");
+        foreach (JournalEntry journal in _journal)
+        foreach (JournalEntry journalEntry in _journal)
+        {
+            journal.Display();
+            journalEntry.Display();
+        }
     }
 
     public void SaveToFile(string file)
     {
+        // string fileName = "MyJournal.txt";
 
+        if (!File.Exists(fileName))
+        {
+            using (StreamWriter outputFile = new StreamWriter(fileName))
+            {
+                File.CreateText(fileName);
+                outputFile.WriteLine("************** Journal Entries **************\n");
+                foreach (JournalEntry journalEntry in _journal)
+                {
+                    outputFile.WriteLine($"{journalEntry._dateTime}");
+                    outputFile.WriteLine($"Prompt: {journalEntry._journalPrompt}");
+                    outputFile.WriteLine($"Entry: {journalEntry._journalEntry}\n");
+                }
+            }
+            Console.Write("\n*** MyJournal.txt has been created! ***\n");
+        }
+        else
+        {
+            Console.Write("\n*** MyJournal.txt already exits. ***\n");
+            using (StreamWriter outputFile = new StreamWriter(fileName, append: true))
+            {
+                foreach (JournalEntry journalEntry in _journal)
+                {
+                    outputFile.WriteLine($"{journalEntry._dateTime}");
+                    outputFile.WriteLine($"Prompt: {journalEntry._journalPrompt}");
+                    outputFile.WriteLine($"Entry: {journalEntry._journalEntry}\n");
+                }
+            }
+        }
     }
 
     public void LoadFromFile(string file)
     {
 
+        if (File.Exists(fileName))
+        {
+            string text = File.ReadAllText(fileName);
+            Console.WriteLine($"\n{text}");
+        }
     }
 }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -2,8 +2,22 @@ public class PromptGenerator
 {
     public List<string>_prompts;
 
+    public static string[] _prompt = {
+        "Think of a time when you stood up for someone else ",
+        "Think of a time when you did something really difficult ",
+        "Think of a time when you helped someone in need ",
+        "Think of a time when you did something truly selfless ",
+        "Who are people that you appreciate? ",
+        "What are personal strengths of yours? ",
+        "Who are people that you have helped this week? ",
+        "When have you felt the Holy Ghost this month? ",
+        "Who are some of your personal heroes? ",
+    };
+
     public string GetRandomPrompt()
     {
+        var random = new Random();
         return "";
+
     }
 }
